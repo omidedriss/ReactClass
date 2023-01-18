7556dfc34aa9b38c32aad86f1d8c978c94f91ae1
btahery <bahareh.tahery@gmail.com>
2023-01-18 19:06:01 +0330

    add variable

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 var omid;
+var bahar;
 root.render(
   <React.StrictMode>
     <App />
