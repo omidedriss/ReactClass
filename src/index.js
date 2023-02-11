c0ecb1a736bafa20723b275d6a9844ea31307d49
Ahmad <7ahmadhadi.gmail.com>
2023-02-11 18:44:26 +0330

    test

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 var omid;
 var omid2;
+var omid3;
 root.render(
   <React.StrictMode>
     <App />
