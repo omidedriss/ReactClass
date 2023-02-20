0f7d24d38b773ef707ebb544fba0363be2097ecd
milad_esmaeili <miladesmaeili@hotmail.com>
2023-02-20 16:53:56 +0330

    -------

---
+++ src/Component/Alitest.jsx
@@ -0,0 +1,7 @@
+export function Alitest(props) {
+  return <div>gfgfgfhjhfhj {props.name}</div>;
+}
+
+export function Alitest2(props) {
+  return <div>ALIIIIIIIII {props.br}</div>;
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+<<<<<<< Updated upstream
 import hello from "./hello";
 import Learn_1 from "./Component/Test_1";
 
@@ -28,12 +29,25 @@ var Cc = (props) => {
   return <div>{props.tst()}</div>;
 };
 //
+=======
+import ClassTrainTest from "./Component/ClassComponentTrain";
+import Learn from "./Component/TypePropstraining";
+import Condition from "./Component/ConditionTrarining";
+import StateTestLearn from "./Component/StatePropsTrain";
+import {
+  Learn1,
+  Learn2,
+  Learn3,
+} from "./Component/FunctiontrainingWithProbs_Multi_export";
+import { Alitest, Alitest2 } from "./Component/Alitest";
+>>>>>>> Stashed changes
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 function Hello(props) {
   return <div>Hello {props.name}</div>;
 }
 
+<<<<<<< Updated upstream
 const Hello2 = ({ name }) => <div>Hello {name}</div>;
 const click = (name) => {
   alert("hi");
@@ -46,6 +60,41 @@ var test2;
 //   render(){
 //      return <div>Hello {this.props.name}</div>
 //   }
+=======
+/////////////////////////////////// without probs
+// function Milad(){
+// return<div>milad majidi</div>
+// }
+// function milad() {
+//   return <div>Learn Test 1</div>; //بدون ورودی
+// }
+// const Amir = () => {
+//   return <div> amir ALI</div>;
+// };
+
+// const milad1 = () => {
+//   return <div>Learn Test 2</div>; //arow function
+// };
+
+// const Learn3 = () => {
+//   return (
+//     <div>
+//       <div>
+//         <Learn2 />
+//         <p>test learn 3</p>
+//       </div>
+//     </div>
+//   ); //تو در تو
+// };
+// ///////////////////////////////////
+
+///////////////////// with probs
+// function FunctionTest(){
+//   return<div>123456789</div>;
+// }
+// const Milad =(props) =>{
+//   return <div>milad ali mohamd {props.amir()}</div>
+>>>>>>> Stashed changes
 // }
 
 var bahar;
@@ -54,6 +103,7 @@ var zahra;
 var morvarid;
 root.render(
   <React.StrictMode>
+<<<<<<< Updated upstream
     <div>
       ss
       <hello name="omid" age="35" handler={() => click("omid")} />
@@ -62,6 +112,10 @@ root.render(
     <Mmilad />
     <Cc tst={Call_1} />
     <Learn_1 />
+=======
+    <Alitest name="ffffff" />
+    <Alitest2 br="khar ast" />
+>>>>>>> Stashed changes
   </React.StrictMode>
 );
 
