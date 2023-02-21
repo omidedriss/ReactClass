eafe5a97ef22fa7410b41ceabab86b4688cbc8f0
omid <eng.omidedriss@gmail.com>
2023-02-21 16:17:40 +0330

    edite

--- src/Component/List.js
+++ src/Component/List.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React,{useState} from "react";
 const List = (props) => {
-  const numbers = [1, 1, 3, 4, 5, 6];
-  const listItem = numbers.map((x, index) => {
+  const numbers = [1,1, 3, 4, 5, 6];
+  let test=[];
+  const [count, setCount] = useState([]);
+
+  const listItem = numbers.map((x,index) => {
     console.log(index);
-    return <li key={index}>{x}</li>;
+    return <li key={index} >{x}</li>;
   });
   return (
     <div>
