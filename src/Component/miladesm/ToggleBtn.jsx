ff60a1a08db4e585ad7d4e338917302d3c164598
milad_esmaeili <miladesmaeili@hotmail.com>
2023-02-25 17:33:25 +0330

    fgfhgfhg

--- src/Component/miladesm/ToggleBtn.jsx
+++ src/Component/miladesm/ToggleBtn.jsx
@@ -1,22 +1,40 @@
 import React, { useState } from "react";
+import "./mystyle.css";
+
+import Button from "./mystyle.js";
 
 const ToggleBtn = () => {
+  const mylist = ["ali", "milad", "hasoode pelastiki", "mohsen"];
   const [status, setStatus] = useState(false);
   const [value, setValue] = useState("On");
   const handleChange = (e) => {
     if (status) {
-      console.log(e.target.value);
+      console.log(e.target.innerHTML);
       setStatus(false);
       setValue("On");
     } else {
-      console.log(e.target.value);
+      console.log(e.target.innerHTML);
       setStatus(true);
       setValue("Off");
     }
   };
   return (
     <div>
-      <button onClick={handleChange}>{value}</button>
+      <button className="teststyle" onClick={handleChange}>
+        {value}
+      </button>
+      <ul>
+        {mylist.map((c, index) => {
+          return <li key={index}>{c}</li>;
+        })}
+      </ul>
+
+      {mylist.map((c, index) => {
+        return <Button key={index}>{c}</Button>;
+      })}
+
+      <Button>Normal</Button>
+      <Button primary>Primary</Button>
     </div>
   );
 };
