0e654fca9a8e43e6c66bdc686d4ffec42ec1203a
miladesm <miladesmaeili@hotmail.com>
2023-02-26 14:33:37 +0330

    dddddd

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import ToggleBtn from "./Component/miladesm/ToggleBtn";
+import Calculator from "./Components/Calculator.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //https://jsbin.com
@@ -65,7 +65,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <ToggleBtn />
+    <Calculator />
   </React.StrictMode>
 );
 reportWebVitals();
