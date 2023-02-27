98e25996f61b62838d3e0e8c2c7a44e802994142
Milad Esmaeili <miladesmaeili@hotmail.com>
2023-02-27 10:47:41 +0330

    tamrin calc

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -8,6 +8,7 @@ import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import "./Calculator.css";
 import { useState } from "react";
+import * as math from 'mathjs';
 import PropTypes from "prop-types";
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -21,23 +22,26 @@ const Item = styled(Paper)(({ theme }) => ({
 
 
 export default function Calculator() {
-  
-  const show_value = () => {
-    if (result !== undefined && result !== null  ) {
-      var res=result +currentResult;
-       
-      setResult(res);
-     // console.log(res);
-      
-      // if (true) {
-      //   //alert(eval(last_result));
-      // }
-      // else {
-      //   //alert(last_result);
-      // }
+  const Is_valid = (expr)=>{
+    try{
+        math.parse(expr);
+        return true;
     }
-    else
-    {
+    catch(ex){
+        return false;
+    }
+  }
+  const show_value = () => {
+    if (result !== undefined && result !== null && currentResult !== 0  ) {
+      var last_res = result + currentResult;
+      if (Is_valid(last_res)) {
+        setCurrentResult(eval(last_res));
+      }
+      else {
+        alert('ورودی نامعتبر');
+      }
+    }
+    else {
       alert('ورودی نامعتبر');
     }
   }
@@ -45,41 +49,41 @@ export default function Calculator() {
   const handleClick_op = (e) => {
     var value = e.currentTarget.value;
     //console.log(e.currentTarget.value);
-    var res=result +currentResult;
+    var res = result == "" ? currentResult : currentResult == 0 ? result : result + currentResult;
     switch (value) {
-      case "=":       
+      case "=":
+        //setCurrentResult(0);
         setResult(res);
-        setCurrentResult(0);   
-        setCurrentResult(show_value);       
+        // setCurrentResult(0);
+        // setCurrentResult(show_value);
+        show_value();
         break;
       case "-":
         // result=setResult(Number(result).toFixed(1));
-        setResult(res);
-        setResult(result+"-");
+        setResult(res + "-");
+        setCurrentResult(0);
+        //setResult(result + "-");
         break;
       case "+":
-        setResult(res);
-        setResult(result+"+");
+        //setResult(res);
+        setResult(res + "+");
+        setCurrentResult(0);
         break;
       case "*":
-        setResult(res);
-        setResult(result+"*");
+        // setResult(res);
+        setResult(res + "*");
+        setCurrentResult(0);
         break;
       case "/":
-        setResult(res);
-        setResult(result+"/");
+        // setResult(res);
+        setResult(res + "/");
+        setCurrentResult(0);
         break;
       case ".":
-        setResult(res);
-        setResult(result+".");
-        // if (!isNaN(+value)) {
-
-        // }
-        //  var char=".";
-        //  debugger
-        //  if (!result.includes(char)) {
-        //esult=setResult(Number(result).toFixed(1));
-        // }
+        //setResult(res);
+        //setResult(res + ".");
+        var _temp_res = currentResult + ".";
+        setCurrentResult(_temp_res); 
         break;
       // default:
       //   setResult("");
@@ -89,24 +93,11 @@ export default function Calculator() {
 
   const handleClick_set_values = (e) => {
     var value = e.currentTarget.value;
-    //console.log(e.currentTarget.value);
-
-   // setCurrentResult(value);     
-    
-
-
-    if (result=="") {
-      setResult(value);    
-      setCurrentResult(value);   
-      
-    }
-    else{
-      setCurrentResult(value);     
-    }
+    debugger
+    var cur_res = currentResult == 0 ?  value  : currentResult + value;
+    setCurrentResult(cur_res); 
   }
 
-
- 
   const [currentResult, setCurrentResult] = useState(0);
   const [result, setResult] = useState("");
   return (
@@ -117,13 +108,12 @@ export default function Calculator() {
       <Grid item xs={4}></Grid>
       <Grid item xs={4}>
         <Item>
-          <TextField
-            step={0.01}
-focused={true}
+          <TextField             
+            focused={true}
             className="result_box_class"
             id="result_box"
             label={result}
-            variant="outlined"            
+            variant="outlined"
             value={currentResult}
           />
         </Item>
@@ -134,7 +124,7 @@ focused={true}
             columns={{ xs: 12, sm: 12, md: 12 }}
           >
             <Grid item xs={12} sm={12} md={12} key="AC">
-              <Button className="calc_btn_class" variant="contained" key="AC" onClick={() => { setResult("");setCurrentResult(0); }}>
+              <Button className="calc_btn_class" variant="contained" key="AC" onClick={() => { setResult(""); setCurrentResult(0); }}>
                 AC
               </Button>
             </Grid>
@@ -159,7 +149,7 @@ focused={true}
                 </Button>
               </Grid>
             ))}
-            <Grid item xs={2} sm={3} md={3}>
+            {/* <Grid item xs={2} sm={3} md={3}>
               <Button
                 onClick={handleClick_op}
                 className="calc_btn_class"
@@ -169,7 +159,7 @@ focused={true}
               >
                 .
               </Button>
-            </Grid>
+            </Grid> */}
 
             <Grid item xs={2} sm={3} md={3}>
               <Button
@@ -215,7 +205,7 @@ focused={true}
                 /
               </Button>
             </Grid>
-            <Grid item xs={2} sm={3} md={3}>
+            <Grid item xs={2} sm={6} md={6}>
               <Button
                 onClick={handleClick_op}
                 className="calc_btn_class"
