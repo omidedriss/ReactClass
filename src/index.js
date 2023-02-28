b79d5f7c6aaaf6782cd15babb5ca67e064f065bb
omid <eng.omidedriss@gmail.com>
2023-02-28 15:15:36 +0330

    edite

--- src/index.js
+++ src/index.js
@@ -78,7 +78,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Event />
+    <CheckLifeCycle />
   </React.StrictMode>
 );
 reportWebVitals();
