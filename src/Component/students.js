b511c79a27ae9b6604b75c6a24b525b659a62b88
Ahmad <7ahmadhadi.gmail.com>
2023-04-19 21:27:14 +0430

    students changed

--- src/Component/students.js
+++ src/Component/students.js
@@ -1,4 +1,23 @@
-import React from "react";
-import Student from "./student";
+import React, { useEffect } from "react";
+import Student from "./Student";
 import "./studentStyle.css";
 import PropTypes from "prop-types";
+
+let students = props.studentsList.map((student, index) => (
+    <Student
+      key={index}
+      id={student.id}
+      name={student.name}
+      classNumber={student.classNumber}
+      phoneNumber={student.phoneNumber}
+      email={student.email}
+      nameChanged={(event) => props.nameChanged(event, student.id)}
+      deleted={props.deleted}
+    />
+  ));
+  if (props.toggle) {
+    return <div className="student-section">{students}</div>;
+  }
+  return students;
+};
+export default React.memo(Students);
