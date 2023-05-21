168b1027292b890e2e18cedb25dba8b7c5829e50
Ahmad <7ahmadhadi.gmail.com>
2023-05-21 21:07:50 +0430

    loader

---
+++ src/Component/Loader/Loader.js
@@ -0,0 +1,13 @@
+import  "./Loader.css";
+const Loader = ()=>{
+
+return(
+    <body style={{backgroundColor:"black"}}>
+
+     <div  className="loader"></div>
+    </body>
+)
+
+}
+
+export default Loader;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import APITest from './API/FirstAPI';
+import Loader from './Component/Loader/Loader';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -28,7 +29,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <React.StrictMode>
 
-   <APITest/>
+   <Loader/>
       
     
    
