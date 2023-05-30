244515a5848909eba106cf286e4bda433ac9a024
omid <eng.omidedriss@gmail.com>
2023-05-30 16:44:28 +0330

    add

--- src/TypeScript/AddTypeScript1.js
+++ src/TypeScript/AddTypeScript1.js
@@ -6,3 +6,7 @@
 //add
 //npm install --save typescript @types/node @types/react @types/react-dom @types/jest
 //yarn add typescript @types/node @types/react @types/react-dom @types/jest
+
+
+//next
+//npx create-next-app@latest nextjs-blog --use-npm --example "https://github.com/vercel/next-learn/tree/master/basics/learn-starter"
---
+++ src/index.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import Status from "./TypeScript/Status6";
+// import StudentsComponent from "./Component/SampleStudent/UseRefComponent";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//https://jsbin.com
+/////////////////////////////////// without probs
+
+// function Learn1() {
+//   return <div>Learn Test 1</div>;
+// }
+
+// const Learn2 = () => {
+//   return <div>Learn Test 2</div>;
+// };
+
+// const Learn3 = () => {
+//   return (
+//     <div>
+//       {/* hello {Math.floor(Math.random()*40)} */}
+//       <div>
+//         <Learn2 />
+//         <p>test learn 3</p>
+//       </div>
+//     </div>
+//   );
+// };
+///////////////////////////////////
+
+///////////////////// with probs
+// function Learn4(props) {
+//   return (
+//     <div>
+//       Learn Test 1 {props.name} {props.age}
+//     </div>
+//   );
+// }
+// function FunctionTest() {
+//   return <div>Learn Test props value</div>;
+// }
+// const Learn5 = (props) => {
+//   return <div>Learn Test 2 {props.testF()}</div>;
+// };
+
+// const Learn6 = (props) => {
+//   return <div>{props.children}</div>;
+// };
+
+// const Learn7 = (props) => {
+//   return (
+//     <div>
+//       <div>
+//         <Learn3>
+//           <p>test learn children</p>
+//         </Learn3>
+//         <Learn2 />
+//         <Learn5 testF={FunctionTest} />
+//         <p>test learn 4 {props.name}</p>
+//       </div>
+//     </div>
+//   );
+// };
+/////////////////////
+
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Status status="loading"  />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+reportWebVitals();
